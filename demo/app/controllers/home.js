fca708bd9f0370192624f35ac4b0242590fcaff6
MiracleBlue <foreverskybound@gmail.com>
2013-09-13 21:14:48 +1000

    Added automatic data for empty models

--- demo/app/controllers/home.js
+++ demo/app/controllers/home.js
@@ -3,10 +3,22 @@ var mongoose = require('mongoose'),
 
 exports.index = function (req, res) {
 	Article.find(function (err, articles) {
+		if (!articles.length) {
+			var foo = new Article({
+				title: "Foo title",
+				url: "footest",
+				text: "Foo text value goes here"
+			});
+			foo.save(function(err, newfoo) {
+				if (err) console.log("Fail!");
+				else console.log("Win!");
+			});
+		}
 		if (err) throw new Error(err);
 		res.render('home', {
 			title: 'Generator-Express MVC',
 			articles: articles
 		});
+		console.log(articles);
 	});
 };
