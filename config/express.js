23b27d1e112f9e7d5a90f0db5f718f8047da629c
MiracleBlue <foreverskybound@gmail.com>
2013-09-13 19:32:11 +1000

    added all the things!

---
+++ config/express.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var hbs = require("express3-handlebars");
+
+module.exports = function(app, config) {
+  app.configure(function () {
+    app.use(express.compress());
+    app.use(express.static(config.root + '/public'));
+    app.set('port', config.port);
+    app.set('views', config.root + '/app/views');
+	  app.engine("handlebars", hbs({defaultLayout: "main", layoutsDir: "app/views/layouts/"}));
+    app.set('view engine', 'handlebars');
+    app.use(express.favicon(config.root + '/public/img/favicon.ico'));
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(function(req, res) {
+      res.status(404).render('404', { title: '404' });
+    });
+  });
+};
